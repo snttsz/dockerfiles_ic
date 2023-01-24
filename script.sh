af672fe76536fe2c85d9c8d0c32057d1b1612efe
sntt <109248112+xsatellit@users.noreply.github.com>
2023-01-24 16:54:00 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo Quantidade de cores do processador a serem utilizadas:
+echo Quantidade de cores do processador a serem utilizados:
 read core
 docker run --rm -it -d -e QTDCORE=${core} --volume="$PWD"/bahiart:/home/bahiart:rw --volume="$PWD"/libsetplay:/home/libsetplay:rw --name build builder
 docker logs --follow build
