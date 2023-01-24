75595781ceb6c9d1288b1d415974c255d148365b
sntt <109248112+xsatellit@users.noreply.github.com>
2023-01-24 17:17:12 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ read core
 if [ ! ${core} ]; then
 	echo Quantidade de cores não definida. Serão utilizados 8 por padrão.
 	core=8
+	sleep 2
 fi
-sleep 2
 docker run --rm -it -d -e QTDCORE=${core} --volume="$PWD"/bahiart:/home/bahiart:rw --volume="$PWD"/libsetplay:/home/libsetplay:rw --name build builder
 docker logs --follow build
